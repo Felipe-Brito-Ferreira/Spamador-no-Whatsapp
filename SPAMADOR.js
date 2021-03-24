4d77d9e2136446a5235b7900dd70803cddee13f4
Felipe-Brito-Ferreira <77641195+Felipe-Brito-Ferreira@users.noreply.github.com>
2021-03-24 20:55:04 -0300

    Add files via upload

---
+++ SPAMADOR.js
@@ -0,0 +1,38 @@
+function enviarScript(scriptText){
+        var allText = scriptText;
+        lines = allText.split("\n"); 
+
+        var i = 0;                  
+        function myLoop() {         
+            setTimeout(function() {   
+                if(lines[i].trim() != ""){
+                    console.log(lines[i]); 
+
+                    window.InputEvent = window.Event || window.InputEvent;
+
+                    var event = new InputEvent('input', {
+                        bubbles: true
+                    });
+
+                    var textbox = document.querySelector('div._1awRl[data-tab="6"]');
+
+                    textbox.textContent = lines[i];
+
+                    textbox.dispatchEvent(event);
+
+                    document.querySelector("button._2Ujuu").click();
+
+                }
+                i++;                    
+                if (i < lines.length) {           
+                    myLoop();             
+                }                       
+            }, 250)
+        }
+
+        myLoop(); 
+    }
+
+    enviarScript(` 
+
+`);
